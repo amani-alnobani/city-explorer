c5fe74de7034fd6696fd373a3e5e8c21e27c2557
amani-alnobani <amanialnobani@gmail.com>
2021-09-05 18:02:30 +0300

    adding map img

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export class App extends Component {
     super(props);
     this.state = {
       locationName: '',
-      locationData: {}
+      theLocationData: {}
     }
   }
 
@@ -18,28 +18,32 @@ export class App extends Component {
     e.preventDefault();
     const url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.locationName}&format=json`;
 
-    console.log(url);
-
-    const response = await axios.get(url);
+    const responseOperation = await axios.get(url);
     this.setState({
-      locationData: response.data[0]
+      theLocationData: responseOperation.data[0]
     });
   }
 
   render() {
+    const styling = {
+      backgroundcolor: "white",
+      fontfamily: "Arial",
+      color: "blue",
+      margin:"20px"
+    }
+
+
     return (
       <div>
-        <form onSubmit={this.handelSubmit}>
+        <form style={styling} onSubmit={this.handelSubmit}>
           <input type="text" onChange={this.handelLocationNameChange} placeholder="enter city name" />
           <input type="submit" value="Explorer!" />
         </form>
-
-        <div>
-          <h2>Location Info</h2>
-          <p>{this.state.locationData.display_name}</p>
-          <p>lat: {this.state.locationData.lat}</p>
-          <p>lon: {this.state.locationData.lon}</p>
-        </div>
+        <h2>Location Info</h2>
+        <p>{this.state.theLocationData.display_name}</p>
+        <p>lat: {this.state.theLocationData.lat}</p>
+        <p>lon: {this.state.theLocationData.lon}</p>
+        <img src={`https://maps.locationiq.com/v3/ataticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.theLocationData.lat},${this.state.theLocationData.lon}&zoom=1-18`} alt="" />
       </div>
     )
   }
